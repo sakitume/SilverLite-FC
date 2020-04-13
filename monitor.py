1d1e30e2e0c404b224acd2e9cce28742b93fd401
Donovan Mandap <donovan@coremechanic.com>
2020-04-12 22:22:53 -0700

    Improved getch() for Mac OS (should be non-blocking)

--- monitor.py
+++ monitor.py
@@ -25,18 +25,28 @@ if sys.platform.startswith('win'):
             return msvcrt.getch()
 else:
     # See: https://stackoverflow.com/questions/13207678/whats-the-simplest-way-of-detecting-keyboard-input-in-python-from-the-terminal
-    # and: https://www.jonwitts.co.uk/archives/896
-    import tty, os, termios
+    import termios, fcntl, os, termios
     def getch():
         fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+        c = None
+
         try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-    
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+            c = sys.stdin.read(1)
+        except IOError: pass
+
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+        return c
 
 def handlePacket(cmdID, packet):
     if cmdID == 0xFF:
