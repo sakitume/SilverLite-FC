114e75c3659bc79c9fae84aa2a0cede3b4e5bea4
Donovan Mandap <donovan@coremechanic.com>
2020-12-18 00:32:37 -0800

    Configure onboard LED pin to have "LED" label so drv_led.c can access it New test_basic_1_led.cpp, source.mk adjusted to build it and other required sources

--- Test/source.mk
+++ Test/source.mk
@@ -6,12 +6,22 @@ DEBUG_OPTIMISED_SRC  := ""
 #-------------------------------------------------------------------------------
 # Source Files
 #-------------------------------------------------------------------------------
+#SOURCES =  \
+#	$(wildcard $(BASE_DIR)/Core/Src/*.c) \
+#	$(wildcard $(BASE_DIR)/SilverLite/*.c) \
+#	$(wildcard $(BASE_DIR)/SilverLite/*.cpp) \
+#	$(wildcard $(BASE_DIR)/SilverLite/rx_afhds2a/*.c) \
+#	$(wildcard $(BASE_DIR)/SilverLite/rx_afhds2a/*.cpp) \
+
 SOURCES =  \
-	$(wildcard $(BASE_DIR)/Core/Src/*.c) \
-	$(wildcard $(BASE_DIR)/SilverLite/*.c) \
-	$(wildcard $(BASE_DIR)/SilverLite/*.cpp) \
-	$(wildcard $(BASE_DIR)/SilverLite/rx_afhds2a/*.c) \
-	$(wildcard $(BASE_DIR)/SilverLite/rx_afhds2a/*.cpp) \
+	$(BASE_DIR)/Core/Src/drv_time.c \
+	$(BASE_DIR)/Core/Src/drv_led.c \
+	$(BASE_DIR)/Core/Src/drv_reset.c \
+	$(BASE_DIR)/Test/test_usb.cpp \
+	$(BASE_DIR)/SilverLite/delay.cpp \
+
+#	$(BASE_DIR)/SilverLite/console.cpp \
+#	$(BASE_DIR)/SilverLite/jee.cpp
 
 #-------------------------------------------------------------------------------
 # Include directories
---
+++ Test/test_basic_1_led.cpp
@@ -0,0 +1,58 @@
+// https://www.st.com/resource/en/datasheet/stm32f303vc.pdf
+// https://www.st.com/resource/en/reference_manual/dm00031020-stm32f405-415-stm32f407-417-stm32f427-437-and-stm32f429-439-advanced-arm-based-32-bit-mcus-stmicroelectronics.pdf
+// https://www.st.com/resource/en/reference_manual/dm00043574-stm32f303xb-c-d-e-stm32f303x6-8-stm32f328x8-stm32f358xc-stm32f398xe-advanced-arm-based-mcus-stmicroelectronics.pdf
+#include "_my_config.h"
+#include "console.h"
+
+extern "C" {
+#include "drv_time.h"
+#include "drv_reset.h"
+#include "drv_led.h"
+}
+
+
+float looptime; // in seconds
+uint32_t lastlooptime;
+uint32_t used_loop_time;
+uint32_t max_used_loop_time;
+
+static void loop()
+{
+    static uint8_t onOffState;
+#if 0    
+    static uint32_t lastTime;
+    uint32_t now = gettime();
+    if ((now - lastTime) >= (1000 * 500))
+#else
+    delay(1000 * 500);
+#endif
+    {
+        if (onOffState)
+        {
+            ledoff();
+        }
+        else
+        {
+            ledon();
+        }
+        onOffState = !onOffState;
+    }
+}
+
+extern "C" void usermain(void)
+{
+	ledoff();
+	delay( 1000 );
+
+    delay (1000 * 500);
+    ledon();
+
+	time_init();
+
+	lastlooptime = gettime() - LOOPTIME;
+    while (true)
+    {
+//        ledflash( 500000 , 8 );
+		loop();
+    }
+}
