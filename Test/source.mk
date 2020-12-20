4caee91cae625b4fe7d7c4b4a2930a06a44bd07b
Donovan Mandap <donovan@coremechanic.com>
2020-12-20 15:28:19 -0800

    Move usb support folders out of Targets/OMNIBUS to new Targets/F3_USB_Support folder so they can also be used by CRAZYBEEF3FS target Adjust source.mk to take the folder change into account

--- Test/source.mk
+++ Test/source.mk
@@ -108,16 +108,16 @@ DEFINES=\
 #-------------------------------------------------------------------------------
 ifeq ($(IS_F3_TARGET), 1)
 SOURCES += \
-	$(wildcard $(BASE_DIR)/Targets/OMNIBUS/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/*.c) \
-	$(wildcard $(BASE_DIR)/Targets/OMNIBUS/Middlewares/ST/STM32_USB_Device_Library/Core/Src/*.c) \
-	$(wildcard $(BASE_DIR)/Targets/OMNIBUS/USB_DEVICE/App/*.c) \
-	$(wildcard $(BASE_DIR)/Targets/OMNIBUS/USB_DEVICE/Target/*.c)
+	$(wildcard $(BASE_DIR)/Targets/F3_USB_Support/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/*.c) \
+	$(wildcard $(BASE_DIR)/Targets/F3_USB_Support/Middlewares/ST/STM32_USB_Device_Library/Core/Src/*.c) \
+	$(wildcard $(BASE_DIR)/Targets/F3_USB_Support/USB_DEVICE/App/*.c) \
+	$(wildcard $(BASE_DIR)/Targets/F3_USB_Support/USB_DEVICE/Target/*.c)
 
 INCLUDE_DIRS+=\
-	$(BASE_DIR)/Targets/OMNIBUS/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc \
-	$(BASE_DIR)/Targets/OMNIBUS/Middlewares/ST/STM32_USB_Device_Library/Core/Inc \
-	$(BASE_DIR)/Targets/OMNIBUS/USB_DEVICE/App \
-	$(BASE_DIR)/Targets/OMNIBUS/USB_DEVICE/Target
+	$(BASE_DIR)/Targets/F3_USB_Support/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc \
+	$(BASE_DIR)/Targets/F3_USB_Support/Middlewares/ST/STM32_USB_Device_Library/Core/Inc \
+	$(BASE_DIR)/Targets/F3_USB_Support/USB_DEVICE/App \
+	$(BASE_DIR)/Targets/F3_USB_Support/USB_DEVICE/Target
 endif
 
 
