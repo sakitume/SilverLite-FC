2213f730c42c5d5b1aa040ffb5d9cc41a7773320
Donovan Mandap <donovan@coremechanic.com>
2020-12-21 00:01:01 -0800

    test_6_flysky functional

--- Test/source.mk
+++ Test/source.mk
@@ -11,7 +11,8 @@ DEBUG_OPTIMISED_SRC  := ""
 #TEST_CONFIG = test_basic_3_adc
 #TEST_CONFIG = test_basic_4_gpio
 #TEST_CONFIG = test_4_osd
-TEST_CONFIG = test_5_ibus
+#TEST_CONFIG = test_5_ibus
+TEST_CONFIG = test_6_flysky
 
 
 
@@ -86,6 +87,25 @@ SOURCES =  \
 	$(BASE_DIR)/SilverLite/rx_ibus.cpp \
 	$(BASE_DIR)/SilverLite/jee.cpp \
 	$(BASE_DIR)/Test/test_5_ibus.cpp
+else ifeq ($(TEST_CONFIG), test_6_flysky)
+SOURCES =  \
+	$(BASE_DIR)/Core/Src/drv_time.c \
+	$(BASE_DIR)/Core/Src/drv_led.c \
+	$(BASE_DIR)/Core/Src/drv_reset.c \
+	$(BASE_DIR)/Core/Src/drv_adc.c \
+	$(BASE_DIR)/Core/Src/battery.c \
+	$(BASE_DIR)/Core/Src/filter.c \
+	$(BASE_DIR)/Core/Src/util.c \
+	$(BASE_DIR)/SilverLite/delay.cpp \
+	$(BASE_DIR)/SilverLite/f3_console.cpp \
+	$(BASE_DIR)/SilverLite/console.cpp \
+	$(BASE_DIR)/SilverLite/tprintf.cpp \
+	$(BASE_DIR)/SilverLite/drv_osd.cpp \
+	$(BASE_DIR)/SilverLite/rx_flysky.cpp \
+	$(wildcard $(BASE_DIR)/SilverLite/rx_afhds2a/*.c) \
+	$(wildcard $(BASE_DIR)/SilverLite/rx_afhds2a/*.cpp) \
+	$(BASE_DIR)/SilverLite/jee.cpp \
+	$(BASE_DIR)/Test/test_6_flysky.cpp
 endif
 
 #-------------------------------------------------------------------------------
---
+++ Test/test_6_flysky.cpp
@@ -0,0 +1,169 @@
+#include "_my_config.h"
+#include "drv_osd.h"
+#include "rx.h"
+
+#if defined(USE_CONSOLE)
+#include "console.h"
+#endif
+
+
+extern "C" {
+#include "drv_time.h"
+#include "drv_reset.h"
+#include "drv_led.h"
+#include "drv_adc.h"
+#include "battery.h"
+}
+
+
+//------------------------------------------------------------------------------
+extern "C" {
+    // Globals that battery.c references so we'll provide them here
+	float thrsum;       // control.c
+	int idle_offset;    // drv_dshot.c
+
+    extern int packetpersecond; // rx_flysky.cpp
+    extern float vbattfilt; // battery.c
+    extern const char *tprintf(const char* fmt, ...);
+}
+
+//------------------------------------------------------------------------------
+// Expected by afhds2a_support.c, writeEEPROM()
+extern "C" void flash_save(void)
+{
+
+}
+
+//------------------------------------------------------------------------------
+float looptime; // in seconds
+uint32_t lastlooptime;
+uint32_t used_loop_time;
+uint32_t max_used_loop_time;
+
+//------------------------------------------------------------------------------
+static uint32_t osd_time;
+static uint32_t _max_loop_time;
+static void update_osd()
+{
+    // Battery voltage on lower left
+    uint8_t maxRows = osd_get_max_rows();
+    uint8_t row = maxRows - 1;
+    const int vbatt = vbattfilt * 100 + 0.5f;
+    int volts = vbatt/100;
+    const char *s = tprintf("%d.%02dV", volts, vbatt%100);
+    osd_print(row, 1, s);
+
+    row = 1;
+    osd_print(row++, 1, tprintf("LOOPTIME: %3d", max_used_loop_time));
+    osd_print(row++, 1, tprintf("OSD: %3d", osd_time));
+    osd_print(row++, 1, tprintf("PPS: %3d", packetpersecond));
+    osd_refresh();
+}
+
+//------------------------------------------------------------------------------
+void silverlite_init(void)
+{
+#if defined(USE_CONSOLE)
+    console_init();
+#endif
+
+    osd_init();
+    update_osd();
+}
+
+//------------------------------------------------------------------------------
+// This is called every LOOPTIME microseconds
+bool silverlite_update()
+{
+#if defined(USE_CONSOLE)
+    // Make sure USB VCP is updated or risk losing connection
+    console_poll();
+#endif    
+
+    // Compute total elapsed milliseconds
+    static uint32_t ticks;
+    uint32_t ms =  ticks++ / (1000 / LOOPTIME);
+
+    // Every 100ms
+    if (0 == (ms % 100))
+    {
+        uint32_t now = gettime();
+        update_osd();
+        osd_time = gettime() - now;
+
+        // return false to signal to main loop to not count this frame
+        // when determining max loop time.
+        return false;
+    }
+    return true;
+}
+
+//------------------------------------------------------------------------------
+static bool silverlite_postupdate(uint32_t max_used_loop_time)
+{
+    static uint32_t secondTimer;
+    if ((gettime() - secondTimer) >= 1000000)
+    {
+#if defined(USE_CONSOLE)
+        console_openPacket();
+        console_appendPacket16(max_used_loop_time);
+        console_appendPacket16(packetpersecond);
+        console_closePacket(0x05);
+#endif
+        secondTimer=  gettime();
+        return true;    // true causes caller to reset max_used_loop_time
+    }
+    
+    return false;
+}
+
+//------------------------------------------------------------------------------
+extern "C" void usermain(void)
+{
+	ledoff();
+	delay( 1000 );
+	time_init();
+	adc_init(); // DMA takes about 1 us once installed. Must be done early, adc is used in battery_init().
+
+    silverlite_init();
+
+	rx_init();
+	battery_init(); // Must be called before gyro_cal() to send initial battery voltage there.
+
+	lastlooptime = gettime() - LOOPTIME;
+
+	while ( true ) { // main loop
+		const uint32_t loop_start_time = gettime();
+		looptime = ( loop_start_time - lastlooptime ) * 1e-6f;
+		lastlooptime = loop_start_time;
+
+		battery();
+		checkrx();
+
+#ifdef USE_SILVERLITE
+		if (silverlite_update()) {
+#endif
+		// for debug
+		used_loop_time = gettime() - loop_start_time;
+		if ( used_loop_time > max_used_loop_time ) {
+			max_used_loop_time = used_loop_time;
+		}
+#ifdef USE_SILVERLITE
+		}
+#endif
+
+		static uint32_t next_loop_start = 0;
+		if ( next_loop_start == 0 ) {
+			next_loop_start = loop_start_time;
+		}
+		next_loop_start += LOOPTIME;
+
+#ifdef USE_SILVERLITE
+		if (silverlite_postupdate(max_used_loop_time))
+		{
+            max_used_loop_time = 0;
+		}
+#endif
+		while ( gettime() < next_loop_start );
+	}
+}
