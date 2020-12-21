72655c1dfdd06c5751d798df7389ca5adc23ef3b
Donovan Mandap <donovan@coremechanic.com>
2020-12-21 02:39:07 -0800

    test_7_fmc functional

--- Test/source.mk
+++ Test/source.mk
@@ -12,7 +12,9 @@ DEBUG_OPTIMISED_SRC  := ""
 #TEST_CONFIG = test_basic_4_gpio
 #TEST_CONFIG = test_4_osd
 #TEST_CONFIG = test_5_ibus
-TEST_CONFIG = test_6_flysky
+#TEST_CONFIG = test_6_flysky
+TEST_CONFIG = test_7_fmc
+
 
 
 
@@ -106,6 +108,23 @@ SOURCES =  \
 	$(wildcard $(BASE_DIR)/SilverLite/rx_afhds2a/*.cpp) \
 	$(BASE_DIR)/SilverLite/jee.cpp \
 	$(BASE_DIR)/Test/test_6_flysky.cpp
+else ifeq ($(TEST_CONFIG), test_7_fmc)
+SOURCES =  \
+	$(BASE_DIR)/Core/Src/drv_time.c \
+	$(BASE_DIR)/Core/Src/drv_led.c \
+	$(BASE_DIR)/Core/Src/drv_reset.c \
+	$(BASE_DIR)/Core/Src/drv_adc.c \
+	$(BASE_DIR)/Core/Src/drv_fmc.c \
+	$(BASE_DIR)/Core/Src/battery.c \
+	$(BASE_DIR)/Core/Src/filter.c \
+	$(BASE_DIR)/Core/Src/util.c \
+	$(BASE_DIR)/SilverLite/delay.cpp \
+	$(BASE_DIR)/SilverLite/f3_console.cpp \
+	$(BASE_DIR)/SilverLite/console.cpp \
+	$(BASE_DIR)/SilverLite/tprintf.cpp \
+	$(BASE_DIR)/SilverLite/drv_osd.cpp \
+	$(BASE_DIR)/SilverLite/jee.cpp \
+	$(BASE_DIR)/Test/test_7_fmc.cpp
 endif
 
 #-------------------------------------------------------------------------------
